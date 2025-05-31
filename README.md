a54481eb18bae72e96150624d36370db7b217e65
Jamie McCullough <92225779+AoifeMcCull@users.noreply.github.com>
2025-05-31 16:17:22 -0500

    Added API with login and logout

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 Front-end Engineer take-home exercise for Fetch Rewards. 
 Uses vite and shadcn-ui to streamline development.
 
-To run locally: clone and open in IDE. Run ```npx vite``` and then click the local URI or enter it in your browser.
+To run locally: clone and open in IDE. create .env file in the project root. Add the line "FETCH_API_BASE_URL=https://frontend-take-home-service.fetch.com/" and save. Run ```npm install``` and ```npx vite``` and then click the local URL or enter it in your browser.
+
+Design decisions:
+While keeping persistent auth would reduce login calls and provide a better user experience, the short expiry window and temporary nature of the project makes me lean towards ignoring it.
+For a more permanent project I would use Json Web Tokens due to my familiarity with them.
+
+Using a single api file rather than separate auth, dogs, locations folders and files due to the temporary nature of the project + known, static scope of the project
---
+++ src/api.ts
@@ -0,0 +1,47 @@
+import axios, { type AxiosError, type AxiosInstance, type AxiosResponse } from "axios";
+
+//Interfaces
+interface LoginCredentials {
+    name: string;
+    email: string;
+}
+
+
+
+//Axios inst
+const api: AxiosInstance = axios.create({
+    baseURL: process.env.REACT_APP_API_URL,
+    withCredentials: true,
+});
+
+//Interceptors
+
+api.interceptors.response.use(
+    (response: AxiosResponse) => response,
+    (error: AxiosError) => {
+        if (error.response?.status === 401) {
+            alert("Your session has expired! Please log in again.");
+            window.location.href = "/";
+        }
+        return Promise.reject(error);
+    }
+);
+
+//API methods
+export const authApi = {
+    login: async (credentials: LoginCredentials): Promise<AxiosResponse> => {
+        const { data } = await api.post<AxiosResponse>(
+            "/auth/login",
+            credentials
+        );
+        return data;
+    },
+
+    logout: async (): Promise<void> => {
+        await api.post("/auth/logout");
+        window.location.href = "/";
+    },
+};
+
+//Raw instance just in case
+export default api;
